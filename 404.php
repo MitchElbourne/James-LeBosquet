266ec560d2f3d946014b7c93d31fbc13a937ed79
MitchElbourne <mkelbourne@gmail.com>
2018-11-20 14:02:52 +0000

    removed span

--- 404.php
+++ 404.php
@@ -6,7 +6,6 @@
   <h1>404 Page Not Found</h3>
   <p>The page your are currently looking for does not exist</p>
   <p>Visit our <a href="/">Home Page</a> or <a href="javascript:history.back()">Go Back</a></p>
-  <span></span>
 </div>
 
 <?php
